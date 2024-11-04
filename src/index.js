3ac9818988ebb4ca9da55a0bd5e922dc18dd3b50
moid05 <moidalam005@hotmail.com>
2024-11-04 19:41:42 +0530

    Add Mongoose for MongoDB integration and configure database connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import serverConfig from './config/serverConfig.js';
+import express from "express";
+import serverConfig from "./config/serverConfig.js";
+import connectDB from "./config/dbConfig.js";
 const app = express();
 
-app.listen(serverConfig.PORT, () => {
-    console.log(`Server is running on port ${serverConfig.PORT}`);
+app.listen(serverConfig.PORT, async () => {
+	console.log(`Server is running on port ${serverConfig.PORT}`);
+	await connectDB();
 });
