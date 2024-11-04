01740a8e4181da784b01b7d0a20aa48a9e613ff2
moid05 <moidalam005@hotmail.com>
2024-11-04 20:20:56 +0530

    body parser removed used express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import express from "express";
-import bodyParser from "body-parser";
-
 import serverConfig from "./config/serverConfig.js";
 import connectDB from "./config/dbConfig.js";
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
 
 app.listen(serverConfig.PORT, async () => {
 	console.log(`Server is running on port ${serverConfig.PORT}`);
