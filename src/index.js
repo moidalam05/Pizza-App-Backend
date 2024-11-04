9e1646a87a2b79b5e2ea74cb9e8536db40b8111d
moid05 <moidalam005@hotmail.com>
2024-11-04 20:08:38 +0530

    Add body-parser middleware for request parsing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 import serverConfig from "./config/serverConfig.js";
 import connectDB from "./config/dbConfig.js";
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.text());
 
 app.listen(serverConfig.PORT, async () => {
 	console.log(`Server is running on port ${serverConfig.PORT}`);
