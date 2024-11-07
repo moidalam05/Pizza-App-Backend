567217aa55a2f0d09ce481ca920806d85ed30eb2
moid05 <moidalam005@hotmail.com>
2024-11-07 16:07:02 +0530

    Add product schema for managing product data in the database

---
+++ src/schema/productSchema.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		productName: {
+			type: String,
+			required: [true, "Product name is required"],
+			trim: true,
+			minlength: [5, "Product name should be at least 5 characters long"],
+		},
+		description: {
+			type: String,
+			trim: true,
+			minlength: [
+				10,
+				"Product description should be at least 10 characters long",
+			],
+		},
+		productImage: {
+			type: String,
+		},
+		price: {
+			type: Number,
+			required: [true, "Product price is required"],
+		},
+		category: {
+			type: String,
+			enum: ["Veg", "Non-Veg", "Drinks", "Sides"],
+			default: "Veg",
+		},
+		inStock: {
+			type: Boolean,
+			required: [true, "Product stock is required"],
+			default: true,
+		},
+		size: {
+			type: String,
+			enum: ["Small", "Medium", "Large"],
+			default: "Medium",
+		},
+	},
+	{ timestamps: true, versionKey: false }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
