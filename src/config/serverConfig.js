4bec8056e11386e6be94b3199471db64b27c046d
moid05 <moidalam005@hotmail.com>
2024-11-07 16:38:13 +0530

    Add Cloudinary configuration

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,6 +4,13 @@ dotenv.config();
 export default {
 	PORT: process.env.PORT || 3000,
 	MONGODB_URI: process.env.MONGODB_URI,
+
+	// JWT
 	JWT_SECRET: process.env.SECRET_KEY,
 	JWT_EXPIRY: process.env.JWT_EXPIRY,
+
+	// Cloudinary
+	CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
--- src/schema/productSchema.js
+++ src/schema/productSchema.js
@@ -44,3 +44,5 @@ const productSchema = new mongoose.Schema(
 
 const Product = mongoose.model("Product", productSchema);
 export default Product;
+
+
